175bed04e96234ba3dd2784e96bbed81f6775108
Pedro Aldeia <pedroaldeia@tecnico.ulisboa.pt>
2025-05-14 19:31:53 +0100

    test for avg

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,6 +1,7 @@
 import pytest
 from functions.add import execute as add
 from functions.sub import execute as sub
+from functions.avg import execute as avg
 
 def test_add():
     assert add(2, 3) == 5, "Soma de 2 e 3 deve ser 5"
@@ -13,3 +14,9 @@ def test_sub():
     assert sub(3, 5) == -2, "Subtração de 3 por 5 deve ser -2"
     assert sub(0, 0) == 0, "Subtração de 0 por 0 deve ser 0"
     assert sub(-2, -3) == 1, "Subtração de -2 por -3 deve ser 1"
+
+def test_avg():
+    assert avg(3, 9) == 6, "Média entre 3 e 9 deve ser 6"
+    assert avg(0, 0) == 0, "Média entre 0 e 0 deve ser 0"
+    assert avg(2, 10) == 6, "Média entre 2 e 10 deve ser 6"
+    assert avg(1, 19) == 10, "Média entre 1 e 19 deve ser 10"
